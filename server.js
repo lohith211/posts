a46ce36b7c9a2081bb7ce3fc5f5dcadf4f1db981
lohith shet <lohith@vihanafashions.co.in>
2020-06-15 17:47:00 +0530

    second

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 
-// Serve only the static files form the dist directory
+// // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/posts'));
-app.all('*', function (req, res, next) {
-  var origin = req.get('origin');
-  res.header('Access-Control-Allow-Origin', origin);
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// app.all('*', function (req, res, next) {
+//   var origin = req.get('origin');
+//   res.header('Access-Control-Allow-Origin', origin);
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 
 app.get('*', function (req, res) {
 
